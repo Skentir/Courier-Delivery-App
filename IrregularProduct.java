f7938e82b773d35c8165607f3022f7598653bfde
marcnt <44109710+marcnt@users.noreply.github.com>
2019-07-01 22:36:27 +0800

    Even more javadoc

--- IrregularProduct.java
+++ IrregularProduct.java
@@ -7,6 +7,15 @@ public class IrregularProduct extends Item
   private final double height;
   private final double weight;
 
+  /**
+   * Constructs an irregular product.
+   *
+   * @param name the name of the item
+   * @param length the maximum length measure of the item
+   * @param width the maximum width measure of the item
+   * @param height the maximum height measure of the item
+   * @param weight the weight of the item
+   */
   public IrregularProduct(String name, double length, double width, double height, double weight)
   {
     super(name, length, width);
@@ -19,18 +28,33 @@ public class IrregularProduct extends Item
     this.weight = weight;
   }
 
+  /**
+   * Gets the type of the item.
+   *
+   * @return the type, in this case, Irregular Product
+   */
   @Override
   public String getItemType()
   {
     return "Irregular Product";
   }
 
+  /**
+   * Returns the height of the item.
+   *
+   * @return the height, in inches
+   */
   @Override
   public double getHeight()
   {
     return height;
   }
 
+  /**
+   * Returns the weight of the item.
+   *
+   * @return the weight, in kilograms, of the item.
+   */
   @Override
   public double getWeight()
   {
--- RegularProduct.java
+++ RegularProduct.java
@@ -7,6 +7,15 @@ public class RegularProduct extends Item
   private final double height;
   private final double weight;
 
+  /**
+   * Constructs a regular product.
+   *
+   * @param name the name of the item
+   * @param length the length of the item
+   * @param width the width of the item
+   * @param height the height of the item
+   * @param weight the weight of the item
+   */
   public RegularProduct(String name, double length, double width, double height, double weight)
   {
     super(name, length, width);
@@ -19,18 +28,33 @@ public class RegularProduct extends Item
     this.weight = weight;
   }
 
+  /**
+   * Gets the type of the item.
+   *
+   * @return the type, in this case, Regular Product
+   */
   @Override
   public String getItemType()
   {
     return "Regular Product";
   }
 
+  /**
+   * Returns the height of the item.
+   *
+   * @return the height, in inches
+   */
   @Override
   public double getHeight()
   {
     return height;
   }
 
+  /**
+   * Returns the weight of the item.
+   *
+   * @return the weight, in kilograms, of the item.
+   */
   @Override
   public double getWeight()
   {
