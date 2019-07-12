5ebf8ae7a33744a33ea8c57752e879ba0420d286
Kirsten S <daniellesison19@gmail.com>
2019-07-12 11:39:05 +0800

    Merge branch 'master' of https://github.com/Skentir/Courier-Delivery-App

---
+++ Banks/Employee.java
@@ -0,0 +1,43 @@
+public class Employee {
+  private final String name;
+  private final String id;
+  private int numYear;
+  private double salary;
+
+  public Employee(String name, String id, int numYear)
+  {
+    this.name = name;
+    this.id = id;
+    this.numYear = numYear;
+  }
+
+  public double computeSalary()
+  {
+    if (this instanceof Manager)
+    {
+      this.setSalary = numYear * ((Manager) this).getEmployees().size() * 300;
+      return setSalary;
+    }
+    else
+    {
+      this.setSalary = numYear * 500;
+      return setSalary;  
+    }
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public String getSalary()
+  {
+    return name;
+  }
+
+  public String getID()
+  {
+    return id;
+  }
+
+}
